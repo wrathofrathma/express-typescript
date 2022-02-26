82e46ae4f05b82147056a9dd728483aa815a5280
Rathma <rathma@kyso.dev>
2022-02-26 11:35:01 -0500

    Separated app initialization / config from the server loop so we can test directly against the framework in unit tests

--- server.ts
+++ server.ts
@@ -1,28 +1,4 @@
-// Import config to setup the global configuration object.
-import { config } from './config'
-import express from 'express';
-
-const app = express();
-
-/** Setup request body parsing */
-import bodyParser from 'body-parser';
-app.use(bodyParser.json());
-
-/** Import routers from ``app/routes/index`` **/
-import indexRouter from './app/routes/index';
-
-/** Configure application to use routers **/
-app.use('/', indexRouter);
-
-/** Global Middleware Example */
-import ExampleMiddleware from './app/http/middleware/ExampleMiddleware'
-
-/** Using the middleware on all routes */
-app.use(ExampleMiddleware);
-
-/** Using a custom error handler */
-import ExceptionHandler from './app/exceptions/ExceptionHandler';
-app.use(ExceptionHandler);
+import { app, config } from './app';
 
 /** Fire up server **/
 app.listen(config.app.port, () => {
